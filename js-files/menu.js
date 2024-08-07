06525f410ff9df639f89e9a42bb847c65df3acf2
AKB <Ameliakingabujar2002@gmail.com>
2024-08-07 17:05:36 +0200

    Create menu.js

---
+++ js-files/menu.js
@@ -0,0 +1,12 @@
+  /* MENU BAR CODE */
+  function openNav() {
+    console.log("Opening the nav");
+    document.getElementById("mymenu").style.width = "100%";
+  }
+  
+  function closeNav() {
+    console.log("Closing the nav");
+    document.getElementById("mymenu").style.width = "0";
+  }
+
+  /* END OF MENU BAR CODE */
